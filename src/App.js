fa2cdb30106c3d6a3ee1b9e240bf9520c4e0a56a
Anna Nguyen <anhntv.fingroup@gmail.com>
2020-07-14 10:06:46 +0700

    test push tu repo da clone

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          test push tu repo da clone
         </p>
         <a
           className="App-link"
